e0e57f5ca649d90fcfffbba8b8b206f0627b46cc
Rupendra Dhungana <dhunganarupendra03@gmail.com>
2026-09-26 15:58:18 +0545

    create user

---
+++ project/src/user/ReadAlluser.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ReadAlluser = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ReadAlluser
---
+++ project/src/user/ReadSingleUser.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ReadSingleUser = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ReadSingleUser
